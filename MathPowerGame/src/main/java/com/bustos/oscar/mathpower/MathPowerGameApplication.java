f6d50f08570614f70afbe37660c6e7d29f302d6f
OscarIbh <oscaribh2011@gmail.com>
2020-06-22 13:55:04 -0500

    Setup MathPowerGame project.

---
+++ MathPowerGame/src/main/java/com/bustos/oscar/mathpower/MathPowerGameApplication.java
@@ -0,0 +1,13 @@
+package com.bustos.oscar.mathpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathPowerGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathPowerGameApplication.class, args);
+	}
+
+}
